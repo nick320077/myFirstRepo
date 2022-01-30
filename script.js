5d51c421c930bd852e46664d9363bd08e67b128d
nick32007 <kobets32@gmail.com>
2022-01-30 16:17:10 +0200

    вывод в консоль

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const a = "переменная из lesson03";
-const b = "наша ветка lesson001";
-
-const logged = () => {
-    console.log('наконец мы узнали все конанды git');
-};
+console.log('hello world');
